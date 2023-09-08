a4c4d9de8bfcbb593427f2e4c032c010d56b41c0
Raphael Cassio de Souza <rapha.cassio@gmail.com>
2023-09-07 18:36:12 -0700

    :tada: Fifth commit

---
+++ modulo05/aula01-vscode-terminal-regex/aula05-tdd-project-pt03/src/entities/carCategory.js
@@ -0,0 +1,14 @@
+'use strict';
+'use strict';
+const Base = require('./base/base')
+
+class CarCategory extends Base {
+  constructor({ id, name, carIds, price }) {
+    super({ id, name });
+
+    this.carIds = carIds;
+    this.price = price;
+  }
+}
+
+module.exports = CarCategory;

18c91d60e6866f3422940076c287ab9457d43850
Raphael Cassio de Souza <rapha.cassio@gmail.com>
2023-09-08 17:19:08 -0700

    :tada: Sixth commit

--- modulo05/aula02-projeto01/app/src/textProcessorFluentAPI.js
+++ modulo05/aula02-projeto01/app/src/textProcessorFluentAPI.js
@@ -1,3 +1,6 @@
+const Person = require('./person')
+const { evaluateRegex } = require('./util')
+
 // O objetivo do Fluent API é executar tarefas
 // como um pipeline, step by step
 // e no fim, chama o build. MUITO similar ao padrão Builder
@@ -25,11 +28,33 @@ class TextProcessorFluentAPI {
     // g --> global
     // m --> multiline
     // i --> insensitive
-    const matchPerson = /(?<=[contratante|contratada]:\s{1})(?!\s)(.*\n.*?)$/gmi
+    const matchPerson = evaluateRegex(/(?<=[contratante|contratada]:\s{1})(?!\s)(.*\n.*?)$/gmi)
 
     const onlyPerson = this.#content.match(matchPerson)
     
     this.#content = onlyPerson
+
+    return this
+  }
+
+  divideTextInColumns() {
+    const splitRegex = evaluateRegex(/,/)
+
+    this.#content = this.#content.map(line => line.split(splitRegex))
+
+    return this
+  }
+
+  removeEmptyCharacters() {
+    const trimSpaces = evaluateRegex(/^\s+|\s+$|\n/g)
+
+    this.#content = this.#content.map(line => line.map(item => item.replace(trimSpaces, '')))
+
+    return this
+  }
+
+  mapPerson() {
+    this.#content = this.#content.map(line => new Person(line))
 
     return this
   }
